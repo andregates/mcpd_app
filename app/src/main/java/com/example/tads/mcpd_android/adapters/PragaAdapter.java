c05613b31b5664b17cc88ccc075c27635a4172c0
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2018-05-01 22:40:22 -0300

    Design fix (Beta).
    Signed-off-by: joaoGabriel55 <j.quaresmasantos_98@hotmail.com>

---
+++ app/src/main/java/com/example/tads/mcpd_android/adapters/PragaAdapter.java
@@ -0,0 +1,69 @@
+package com.example.tads.mcpd_android.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.tads.mcpd_android.R;
+import com.example.tads.mcpd_android.model.Praga;
+
+import java.util.List;
+
+/**
+ * Created by suelliton on 12/11/2017.
+ */
+
+public class PragaAdapter extends BaseAdapter {
+    Context context;
+    List<Praga> listaPragas;
+    public PragaAdapter(Context context, List<Praga> objects) {
+        super();
+        this.context = context;
+        this.listaPragas = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.praga_adapter_inflate, parent, false);
+            holder.pragaTextView = (TextView) convertView.findViewById(R.id.praga_text);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder)convertView.getTag();
+        }
+
+
+        Praga praga = (Praga) getItem(position);
+
+        holder.pragaTextView.setText(praga.getNome());
+        holder.pragaTextView.setTextSize(25);
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return listaPragas.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return listaPragas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return listaPragas.get(i).getId();
+    }
+
+    class ViewHolder{
+        TextView pragaTextView;
+    }
+
+
+
+}
